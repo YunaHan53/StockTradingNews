8fa6b990e274f8172b676a49244e7e7347ee1aef
YunaH <Yunahan53@gmail.com>
2026-03-03 17:30:28 -0500

    Completed all the TODOs to grab data from Stock API & news API and send message with the news articles details.

--- main.py
+++ main.py
@@ -1,47 +1,70 @@
+from credentials import Credential
+import requests
+from twilio.rest import Client
+
+# Stock API
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
+STOCK_API_KEY = Credential.stock_api_key
+STOCK_ENDPOINT = Credential.stock_endpoint
 
-STOCK_ENDPOINT = "https://www.alphavantage.co/query"
-NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+# News API
+NEWS_API_KEY = Credential.news_api_key
+NEWS_ENDPOINT = Credential.news_endpoint
 
-    ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
-# When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+# Twilio
+account_sid = Credential.twilio_sid
+auth_token = Credential.twilio_auth_token
 
-#TODO 1. - Get yesterday's closing stock price. Hint: You can perform list comprehensions on Python dictionaries. e.g. [new_value for (key, value) in dictionary.items()]
+stock_params = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK_NAME,
+    "interval": "5min",
+    "apikey": STOCK_API_KEY,
+}
+stock_res = requests.get(STOCK_ENDPOINT, params=stock_params)
+stock_res.raise_for_status()
+stock_data = stock_res.json()["Time Series (Daily)"]
 
-#TODO 2. - Get the day before yesterday's closing stock price
+stock_price_list = [value for (key, value) in stock_data.items()]
+yesterday_data = stock_price_list[0]
+yesterday_closing_price = float(yesterday_data["4. close"])
 
-#TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
+two_days_ago_data = stock_price_list[1]
+two_days_ago_closing_price = float(two_days_ago_data["4. close"])
 
-#TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
+price_diff = yesterday_closing_price - two_days_ago_closing_price
 
-#TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
+up_down = None
+if price_diff > 0:
+    up_down = "🔺"
+else:
+    up_down = "🔻"
 
-    ## STEP 2: https://newsapi.org/ 
-    # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
+percentage = round((price_diff / yesterday_closing_price), 2) * 100
 
-#TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
+if abs(percentage) >= 3:
+    news_params = {
+        "q": COMPANY_NAME,
+        "language": "en",
+        "apiKey": NEWS_API_KEY,
+    }
 
-#TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
+    news_res = requests.get(NEWS_ENDPOINT, news_params)
+    news_res.raise_for_status()
+    news_articles = news_res.json()["articles"]
 
+    three_articles_list = news_articles[0:3]
 
-    ## STEP 3: Use twilio.com/docs/sms/quickstart/python
-    #to send a separate message with each article's title and description to your phone number. 
+    articles_summary = [f"Headline: {article['title']} \nBrief: {article['description']}" for article in three_articles_list]
 
-#TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
-
-#TODO 9. - Send each article as a separate message via Twilio. 
-
-
-
-#Optional TODO: Format the message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
-
+    client = Client(account_sid, auth_token)
+    for article in articles_summary:
+        message = client.messages.create(
+            from_="whatsapp:+14155238886",
+            body=f"{STOCK_NAME}: {up_down} {percentage}%\n"
+                 f"{article}",
+            to="whatsapp:+18572588770"
+        )
+        print(message.body)
+        print(message.status)
